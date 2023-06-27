ddb61997a010f7fc092513685015faf85567b9f3
okur <bijay.okur@gmail.com>
2023-06-27 10:34:06 +0545

    build: fav icon added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,6 +11,7 @@ export const metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
+      <link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
       <body>
         <StoreProvider>
           <ThemeProviderRoot>{children}</ThemeProviderRoot>
